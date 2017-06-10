eb05a6181eac314127658287532cbcb2746c8c3b
CirXe0N <dw1ls@outlook.com>
2017-06-10 21:52:13 +0200

    Add Chat functionality

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,8 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild, ViewChildren, QueryList, AfterViewInit} from "@angular/core";
+import {APIService} from "../../services/api.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ChatMessage} from "../../interfaces/chatMessage.interface";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'chat',
@@ -6,7 +10,54 @@ import {Component} from "@angular/core";
   styleUrls: ['./chat.component.sass']
 })
 
-export class ChatComponent {
-  public isConnected: boolean = false;
+export class ChatComponent implements OnInit, AfterViewInit {
+  @ViewChild('messageContainer') private messageContainer: ElementRef;
+  @ViewChildren('messageItems') private messageItems: QueryList<any>;
+
+  public form: FormGroup;
+  public isConnected: boolean;
+  public messages = [];
+
+  constructor(private api: APIService, private fb: FormBuilder, private user: UserService) {
+    this.form = this.fb.group({
+      content: ['', Validators.required]
+    });
+  }
+
+  public ngOnInit() {
+    this.api.getConnectionStatus().subscribe(res => {
+      this.isConnected = res;
+      if(this.isConnected) {
+        this.form.enable()
+      } else {
+        this.form.disable()
+      }
+    });
+
+    this.api.getChatMessages().subscribe(res => this.messages.push(res));
+    this.api.findGameRoom();
+  }
+
+  public ngAfterViewInit() {
+    this.messageItems.changes.subscribe(() => this.scrollToBottom())
+  }
+
+  public sendMessage(message: ChatMessage): void {
+    this.form.controls.content.setValue(this.form.controls.content.value.trim());
+
+    if (this.form.valid) {
+      message.user_id = this.user.getUserID();
+      this.api.sendMessage(message);
+      this.form.controls.content.setValue('')
+    }
+  }
+
+  private scrollToBottom(): void {
+    this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+  }
+
+  public isOpponentMessage(message: ChatMessage): boolean {
+    return message.user_id !== this.user.getUserID()
+  }
 }
 
